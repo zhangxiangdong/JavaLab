5ac69b947339e9ac5c5bfc51bf518552893b7435
zhangxiangdong <zxdeinstein@163.com>
2016-11-23 10:14:58 +0800

    Test hero build

--- DesignPatterns/src/test/java/com/zhang/builder/HeroTest.java
+++ DesignPatterns/src/test/java/com/zhang/builder/HeroTest.java
@@ -2,6 +2,9 @@ package com.zhang.builder;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by zhangxiangdong on 2016/11/23.
  */
@@ -21,6 +24,27 @@ public class HeroTest {
     @Test(expected = IllegalArgumentException.class)
     public void testMissingName() throws Exception {
         new Hero.Builder(Profession.WARRIOR, null);
+    }
+
+    @Test
+    public void testBuildHero() throws Exception {
+        final String heroName = "Sir Lancelot";
+
+        final Hero hero = new Hero.Builder(Profession.MAGE, heroName)
+                .withArmor(Armor.CHAIN_MAIL)
+                .withHairColor(HairColor.BROWN)
+                .withHairType(HairType.CURLY)
+                .withWeapon(Weapon.AXE)
+                .build();
+
+        assertNotNull(hero);
+        assertNotNull(hero.toString());
+
+        assertEquals(Profession.MAGE, hero.getProfession());
+        assertEquals(Armor.CHAIN_MAIL, hero.getArmor());
+        assertEquals(HairColor.BROWN, hero.getHairColor());
+        assertEquals(HairType.CURLY, hero.getHairType());
+        assertEquals(Weapon.AXE, hero.getWeapon());
     }
 
 }
