52433e1442e6505bccbf8a12b7d6fe07912a8cd7
zhangxiangdong <zxdeinstein@163.com>
2016-11-23 09:59:30 +0800

    Test builder app

---
+++ DesignPatterns/src/test/java/com/zhang/builder/AppTest.java
@@ -0,0 +1,15 @@
+package com.zhang.builder;
+
+import org.junit.Test;
+
+/**
+ * Created by zhangxiangdong on 2016/11/23.
+ */
+public class AppTest {
+    @Test
+    public void test() {
+        String[] args = {};
+        App.main(args);
+    }
+
+}
