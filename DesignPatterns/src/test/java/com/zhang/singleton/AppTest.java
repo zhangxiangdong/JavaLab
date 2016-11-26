3c89c48d048314c7174b94739d54a99c08bb6a33
zhangxiangdong <zxdeinstein@163.com>
2016-11-26 20:46:28 +0800

    Test App

---
+++ DesignPatterns/src/test/java/com/zhang/singleton/AppTest.java
@@ -0,0 +1,16 @@
+package com.zhang.singleton;
+
+import org.junit.Test;
+
+/**
+ * Created by Zhang on 11/26/2016 8:36 PM.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        String[] args = {};
+        App.main(args);
+    }
+
+}
