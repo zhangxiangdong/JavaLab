6320dd8489bddc81d91b12db2509e2855762b4cb
zhangxiangdong <zxdeinstein@163.com>
2016-11-28 13:07:27 +0800

    Object pool pattern

---
+++ DesignPatterns/src/main/java/com/zhang/pool/TitanicPool.java
@@ -0,0 +1,13 @@
+package com.zhang.pool;
+
+/**
+ * Created by zhangxiangdong on 2016/11/28.
+ */
+public class TitanicPool extends ObjectPool<Titanic> {
+
+    @Override
+    protected Titanic create() {
+        return new Titanic();
+    }
+
+}
