ddcc05a60e92c2c54993c4c095e372d326847ad9
zhangxiangdong <zxdeinstein@163.com>
2016-11-29 15:56:02 +0800

    Test main

---
+++ DesignPatterns/src/test/java/com/zhang/structural/adapter/AppTest.java
@@ -0,0 +1,16 @@
+package com.zhang.structural.adapter;
+
+import org.junit.Test;
+
+/**
+ * Created by zhangxiangdong on 2016/11/29.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        String[] args = {};
+        App.main(args);
+    }
+
+}
