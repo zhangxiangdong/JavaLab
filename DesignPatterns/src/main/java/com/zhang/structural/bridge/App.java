1765c207a7aebe8e76a9da9f9327bd2bba90fa9d
zhangxiangdong <zxdeinstein@163.com>
2016-12-01 15:50:15 +0800

    Test bridge

---
+++ DesignPatterns/src/main/java/com/zhang/structural/bridge/App.java
@@ -0,0 +1,16 @@
+package com.zhang.structural.bridge;
+
+/**
+ * Created by zhangxiangdong on 2016/12/1.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Shape triangle = new Triangle(new GreenColor());
+        triangle.applyColor();
+
+        Shape pentagon = new Pentagon(new RedColor());
+        pentagon.applyColor();
+    }
+
+}
---
+++ DesignPatterns/src/test/java/com/zhang/structural/bridge/AppTest.java
@@ -0,0 +1,16 @@
+package com.zhang.structural.bridge;
+
+import org.junit.Test;
+
+/**
+ * Created by zhangxiangdong on 2016/12/1.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        String[] args = {};
+        App.main(args);
+    }
+
+}
