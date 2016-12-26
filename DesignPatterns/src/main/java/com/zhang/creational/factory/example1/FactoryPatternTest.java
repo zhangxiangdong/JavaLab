86184765c95101453c8c5cb7052d178b60c43b3e
zhangxiangdong <zxdeinstein@163.com>
2016-12-26 11:25:07 +0800

    Factory pattern, a good way to create an object.

---
+++ DesignPatterns/src/main/java/com/zhang/creational/factory/example1/FactoryPatternTest.java
@@ -0,0 +1,28 @@
+package com.zhang.creational.factory.example1;
+
+/**
+ * Created by zhangxiangdong on 2016/12/26.
+ */
+public class FactoryPatternTest {
+
+    public static void main(String[] args) {
+        ShapeFactory factory = new ShapeFactory();
+
+        Shape rectangle = factory.createANewShape(ShapeType.RECTANGLE);
+        rectangle.draw();
+
+        Shape triangle = factory.createANewShape(ShapeType.TRIANGLE);
+        triangle.draw();
+
+        Shape square = factory.createANewShape(ShapeType.SQUARE);
+        square.draw();
+
+        Shape circle = factory.createANewShape(ShapeType.CIRCLE);
+        circle.draw();
+
+        Shape unknown = factory.createANewShape(ShapeType.UNKNOWN);
+        unknown.draw();
+
+    }
+
+}
---
+++ DesignPatterns/src/main/java/com/zhang/creational/factory/example1/ShapeType.java
@@ -0,0 +1,10 @@
+package com.zhang.creational.factory.example1;
+
+/**
+ * Created by zhangxiangdong on 2016/12/26.
+ */
+public enum ShapeType {
+
+    CIRCLE, RECTANGLE, SQUARE, TRIANGLE, UNKNOWN
+
+}
