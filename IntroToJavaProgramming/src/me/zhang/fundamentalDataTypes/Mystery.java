64ad430edf31b311af65df8bea31a8c15f587dc5
zhangxiangdong <zxdeinstein@163.com>
2017-01-15 15:03:31 +0800

    Overflow

---
+++ IntroToJavaProgramming/src/me/zhang/fundamentalDataTypes/Mystery.java
@@ -0,0 +1,23 @@
+package me.zhang.fundamentalDataTypes;
+
+/**
+ * Created by Zhang on 1/15/2017 2:58 PM.
+ */
+public class Mystery {
+
+    public static void main(String[] args) {
+        System.out.println(getIntegerMystery());
+        System.out.println(getDoubleMystery());
+    }
+
+    private static int getIntegerMystery() {
+        int oneMillion = 1000000;
+        return oneMillion * oneMillion;
+    }
+
+    private static double getDoubleMystery() {
+        double oneMillion = 1000000;
+        return oneMillion * oneMillion;
+    }
+
+}
