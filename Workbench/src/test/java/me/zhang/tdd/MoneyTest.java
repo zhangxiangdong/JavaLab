12cc6003936d0525ae522800022fd445c85d18ad
zhangxiangdong <zxdeinstein@163.com>
2017-02-28 10:19:32 +0800

    Compare different Money

--- Workbench/src/test/java/me/zhang/tdd/MoneyTest.java
+++ Workbench/src/test/java/me/zhang/tdd/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 /**
  * Created by zhangxiangdong on 2017/2/27.
  */
-public class DollarTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() {
@@ -20,6 +20,18 @@ public class DollarTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+
+        assertFalse(new Dollar(5).equals(new Franc(5)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
 }
