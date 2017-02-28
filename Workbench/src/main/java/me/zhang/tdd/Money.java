131216cd1742ab3bca2a41f5e65314fa63fa5b31
zhangxiangdong <zxdeinstein@163.com>
2017-02-28 14:17:46 +0800

    Delete subclasses and eliminate redundant tests

--- Workbench/src/main/java/me/zhang/tdd/Money.java
+++ Workbench/src/main/java/me/zhang/tdd/Money.java
@@ -23,12 +23,12 @@ public class Money {
         return new Money(amount * multiplier, currency);
     }
 
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    public static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 
     public String currency() {
