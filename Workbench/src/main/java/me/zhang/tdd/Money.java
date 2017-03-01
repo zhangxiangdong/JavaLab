3b1396d249e685617d9835d2bc7f942e2d3da0f0
zhangxiangdong <zxdeinstein@163.com>
2017-03-01 11:29:08 +0800

    Add Mixed Currencies

--- Workbench/src/main/java/me/zhang/tdd/Money.java
+++ Workbench/src/main/java/me/zhang/tdd/Money.java
@@ -19,7 +19,7 @@ public class Money implements Expression {
         return amount == money.amount && currency.equals(money.currency);
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -40,7 +40,8 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    public Expression plus(Money addend) {
+    @Override
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
--- Workbench/src/test/java/me/zhang/tdd/MoneyTest.java
+++ Workbench/src/test/java/me/zhang/tdd/MoneyTest.java
@@ -70,4 +70,14 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
 }
