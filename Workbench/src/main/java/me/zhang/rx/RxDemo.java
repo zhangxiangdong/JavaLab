51b6183b9bdaf34acf95c9058b1f9c573b97c0a1
zhangxiangdong <zxdeinstein@163.com>
2017-04-21 17:56:43 +0800

    Create Observable via create method

--- Workbench/src/main/java/me/zhang/rx/RxDemo.java
+++ Workbench/src/main/java/me/zhang/rx/RxDemo.java
@@ -2,6 +2,7 @@ package me.zhang.rx;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by zhangxiangdong on 2017/4/21.
@@ -30,6 +31,49 @@ public class RxDemo {
 
         source.subscribe(integer -> System.out.println("Number -> " + integer), Throwable::printStackTrace,
                 () -> System.out.println("Completed."));
+
+        Observable.create((Observable.OnSubscribe<String>) subscriber -> {
+            try {
+                String result = doSomeTimeTakingIoOperation();
+                subscriber.onNext(result);    // Pass on the data to subscriber
+                subscriber.onCompleted();     // Signal about the completion subscriber
+            } catch (Exception e) {
+                subscriber.onError(e);        // Signal about the error to subscriber
+            }
+        })
+                .observeOn(Schedulers.io())
+                .subscribe(new Subscriber<String>() {
+
+                    @Override
+                    public void onStart() {
+                        super.onStart();
+                        System.out.println("Waiting for result...");
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        System.out.println("Done.");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.println("\t" + s);
+                    }
+                });
+    }
+
+    private static String doSomeTimeTakingIoOperation() {
+        try {
+            Thread.sleep(3 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Results from Mars!";
     }
 
 }
