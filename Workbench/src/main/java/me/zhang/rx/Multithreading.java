d608fd1caa57d439bc24ebd398c364d58229d1e4
zhangxiangdong <zxdeinstein@163.com>
2017-04-24 18:10:51 +0800

    Change execution thread using observeOn

--- Workbench/src/main/java/me/zhang/rx/Multithreading.java
+++ Workbench/src/main/java/me/zhang/rx/Multithreading.java
@@ -3,6 +3,7 @@ package me.zhang.rx;
 import rx.Observable;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.Callable;
 
@@ -12,9 +13,38 @@ import java.util.concurrent.Callable;
 public class Multithreading {
 
     public static void main(String[] args) {
+//        defaultThreading();
+//        operatorOnDifferentThread();
+//        subscriberOnDifferentThread();
+        bothOperatorAndSubscriberOnDifferntThread();
+    }
+
+    private static void defaultThreading() {
         Observable.fromCallable(thatReturnsNumberOne())     // the Observable
                 .map(numberToString())                      // the Operator
                 .subscribe(printResult());                  // the Subscriber
+    }
+
+    private static void operatorOnDifferentThread() {
+        Observable.fromCallable(thatReturnsNumberOne())
+                .observeOn(Schedulers.newThread())      // operator on different thread
+                .map(numberToString())
+                .subscribe(printResult());
+    }
+
+    private static void subscriberOnDifferentThread() {
+        Observable.fromCallable(thatReturnsNumberOne())
+                .map(numberToString())
+                .observeOn(Schedulers.newThread())      // subscriber on different thread
+                .subscribe(printResult());
+    }
+
+    private static void bothOperatorAndSubscriberOnDifferntThread() {
+        Observable.fromCallable(thatReturnsNumberOne())
+                .observeOn(Schedulers.newThread())      // operator on different thread
+                .map(numberToString())
+                .observeOn(Schedulers.newThread())      // subscriber on different thread
+                .subscribe(printResult());
     }
 
     private static Callable<Integer> thatReturnsNumberOne() {
