e5bf3c70cc4f57a9fadebd3c0f543a0adf7e66b7
zhangxiangdong <zxdeinstein@163.com>
2017-04-27 18:47:40 +0800

    Test add/remove entries

--- DataStructuresAndAlgorithms/src/main/java/me/zhang/array/game/Scoreboard.java
+++ DataStructuresAndAlgorithms/src/main/java/me/zhang/array/game/Scoreboard.java
@@ -46,4 +46,45 @@ public class Scoreboard {
         return removed;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int j = 0; j < numEntries; j++) {
+            if (j > 0)
+                sb.append(", ");
+            sb.append(board[j]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // The main method
+        Scoreboard highscores = new Scoreboard(5);
+        String[] names = {"Rob", "Mike", "Rose", "Jill", "Jack", "Anna", "Paul", "Bob"};
+        int[] scores = {750, 1105, 590, 740, 510, 660, 720, 400};
+
+        for (int i = 0; i < names.length; i++) {
+            GameEntry gE = new GameEntry(names[i], scores[i]);
+            System.out.println("Adding " + gE);
+            highscores.add(gE);
+            System.out.println("\tScoreboard: " + highscores);
+        }
+        System.out.println("Removing score at index " + 3);
+        highscores.remove(3);
+        System.out.println("\tScoreboard: " + highscores);
+        System.out.println("Removing score at index " + 0);
+        highscores.remove(0);
+        System.out.println("\tScoreboard: " + highscores);
+        System.out.println("Removing score at index " + 1);
+        highscores.remove(1);
+        System.out.println("\tScoreboard: " + highscores);
+        System.out.println("Removing score at index " + 1);
+        highscores.remove(1);
+        System.out.println("\tScoreboard: " + highscores);
+        System.out.println("Removing score at index " + 0);
+        highscores.remove(0);
+        System.out.println("\tScoreboard: " + highscores);
+    }
+
 }
