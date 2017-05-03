07522fb48c5306b70bd8644ae8dfba844b80820b
zhangxiangdong <zxdeinstein@163.com>
2017-05-03 14:38:49 +0800

    Generate streams

---
+++ Workbench/src/main/java/me/zhang/java8/StreamsTester.java
@@ -0,0 +1,18 @@
+package me.zhang.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by zhangxiangdong on 2017/5/3.
+ */
+public class StreamsTester {
+
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("A", "", "Good", "Example", "", "!");
+        List<String> output = input.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        System.out.println(output);
+    }
+
+}
