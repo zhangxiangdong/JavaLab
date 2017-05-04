3e666cd57ccb05bcd10024a0dc699f9e6ae54c65
zhangxiangdong <zxdeinstein@163.com>
2017-05-04 13:15:42 +0800

    Equivalence Testing with Arrays

--- DataStructuresAndAlgorithms/src/main/java/me/zhang/array/IsArrayAnObjectTester.java
+++ DataStructuresAndAlgorithms/src/main/java/me/zhang/array/IsArrayAnObjectTester.java
@@ -1,5 +1,7 @@
 package me.zhang.array;
 
+import java.util.Arrays;
+
 /**
  * Created by zhangxiangdong on 2017/5/4.
  */
@@ -9,6 +11,16 @@ public class IsArrayAnObjectTester {
         int[] anArray = {0, 1, 2, 3, 4, 5};
         System.out.println(anArray.getClass()); // "class [I"
         System.out.println(anArray.getClass().getSuperclass()); // "class java.lang.Object"
+
+        int[] anotherArray = {0, 1, 2, 3, 4, 5};
+        System.out.println(anArray == anotherArray); // "false"
+        System.out.println(Arrays.equals(anArray, anotherArray)); // "true"
+
+        int[][] twoDArray = {{0, 1}, {2, 3}, {4, 5}};
+        int[][] anotherTwoDArray = {{0, 1}, {2, 3}, {4, 5}};
+        System.out.println(twoDArray == anotherTwoDArray); // "false"
+        System.out.println(Arrays.equals(twoDArray, anotherTwoDArray)); // "false"
+        System.out.println(Arrays.deepEquals(twoDArray, anotherTwoDArray)); // "true"
     }
 
 }
