7cfe9a6cd23cd1822f813c4b9e82309981388ab2
zhangxiangdong <zxdeinstein@163.com>
2017-08-04 11:18:51 +0800

    Count number of 1 in binary

---
+++ Workbench/src/main/java/me/zhang/demo/Bitwise.java
@@ -0,0 +1,34 @@
+package me.zhang.demo;
+
+public class Bitwise {
+
+    /**
+     * 输入整数，输出该数二进制表示中1的个数。
+     * 例如把9表示成二进制是1001，有2位1。因此如果输入9，该方法返回2。
+     *
+     * @param num 输入的整数
+     * @return 返回的二进制中1位的个数
+     */
+    int numberOf1(int num) {
+        int count = 0;
+        int flag = 1;
+        while (flag != 0) {
+            // 遇到1位，计数+1
+            if ((num & flag) != 0) {
+                count++;
+            }
+            flag <<= 1;
+        }
+        return count;
+    }
+
+    int numberOfEfficiently(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            num &= num - 1;
+        }
+        return count;
+    }
+
+}
---
+++ Workbench/src/test/java/me/zhang/demo/BitwiseTest.java
@@ -0,0 +1,34 @@
+package me.zhang.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BitwiseTest {
+
+    private Bitwise bitwise = new Bitwise();
+
+    @Test
+    public void numberOfOne() throws Exception {
+        System.out.println(Integer.toBinaryString(-8));
+        Assert.assertEquals(29, bitwise.numberOf1(-8));
+
+        System.out.println(String.format("%32s", Integer.toBinaryString(0)).replace(' ', '0'));
+        Assert.assertEquals(0, bitwise.numberOf1(0));
+
+        System.out.println(String.format("%32s", Integer.toBinaryString(9)).replace(' ', '0'));
+        Assert.assertEquals(2, bitwise.numberOf1(9));
+    }
+
+    @Test
+    public void numberOfOneEfficient() throws Exception {
+        System.out.println(Integer.toBinaryString(-6));
+        Assert.assertEquals(30, bitwise.numberOfEfficiently(-6));
+
+        System.out.println(String.format("%32s", Integer.toBinaryString(0)).replace(' ', '0'));
+        Assert.assertEquals(0, bitwise.numberOfEfficiently(0));
+
+        System.out.println(String.format("%32s", Integer.toBinaryString(9)).replace(' ', '0'));
+        Assert.assertEquals(2, bitwise.numberOfEfficiently(9));
+    }
+
+}
