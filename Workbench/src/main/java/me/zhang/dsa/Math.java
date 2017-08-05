7dc124aa8c532067f126aeeb5fc9bbb3f7cf4ebb
zhangxiangdong <zxdeinstein@163.com>
2017-08-05 10:30:01 +0800

    Optimise power with unsigned exponent method

--- Workbench/src/main/java/me/zhang/dsa/Math.java
+++ Workbench/src/main/java/me/zhang/dsa/Math.java
@@ -14,7 +14,7 @@ public class Math {
         double result;
         int absExponent = exponent < 0 ? -exponent : exponent;
         // 计算指数为正数的整数次方
-        result = powerWithUnsignedExponent(base, absExponent);
+        result = pwue(base, absExponent);
         // 指数小于0，对计算结果取倒数
         if (exponent < 0) {
             result = 1 / result;
@@ -32,6 +32,50 @@ public class Math {
             result *= base;
         }
         return result;
+    }
+
+    /**
+     * 参考 me.zhang.dsa 包下：a的n次方.png
+     */
+    private double pwue(double base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        if (exponent == 1) {
+            return base;
+        }
+        // 位移操作比/运算符效率高
+        double result = pwue(base, exponent >> 1);
+        result *= result;
+        // 判断exponent是否为奇数（比%运算符效率高）
+        if ((exponent & 1) == 1) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Math math = new Math();
+        System.out.println(math.power(2, 2));
+        System.out.println(math.power(2, -3));
+        System.out.println(math.power(2, 0));
+
+        System.out.println(math.power(-3, 2));
+        System.out.println(math.power(-3, -3));
+        System.out.println(math.power(-3, 0));
+
+        System.out.println(math.power(0, 4));
+        try {
+            System.out.println(math.power(0, -5));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(math.power(0, 0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
