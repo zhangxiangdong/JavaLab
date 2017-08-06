ebe30cd5f0fa3c0d4355cbd250c34041e5db1e55
zhangxiangdong <zxdeinstein@163.com>
2017-08-06 17:16:31 +0800

    Print out all combinations recursively

--- Workbench/src/main/java/me/zhang/string/StringOperator.java
+++ Workbench/src/main/java/me/zhang/string/StringOperator.java
@@ -2,6 +2,8 @@ package me.zhang.string;
 
 import java.util.Arrays;
 
+import static java.lang.Character.isLetterOrDigit;
+
 /**
  * Created by Zhang on 8/6/2017 8:23 AM.
  */
@@ -53,6 +55,65 @@ public class StringOperator {
         }
     }
 
+    public boolean mappingPassword(String password) {
+        final int z = 122;
+        final int zero = 48;
+        boolean found = false;
+        mapping:
+        for (int i = zero; i <= z; i++) {
+            for (int j = zero; j <= z; j++) {
+                for (int k = zero; k <= z; k++) {
+                    char i1 = (char) i;
+                    char j1 = (char) j;
+                    char k1 = (char) k;
+                    char[] guess = {i1, j1, k1};
+                    if (isLetterOrDigit(i1) && isLetterOrDigit(j1) && isLetterOrDigit(k1)) {
+                        String tester = new String(guess);
+                        System.out.print(tester + "\t");
+                        if (password.equals(tester)) {
+                            found = true;
+                            System.out.println();
+                            break mapping;
+                        }
+                    }
+                }
+            }
+        }
+        return found;
+    }
+
+    private boolean found;
+
+    public boolean mappingPasswordRecursively(String password) {
+        char[] guessPassword = new char[3];
+        generatePassword(guessPassword, 0, password);
+        return found;
+    }
+
+    /**
+     * @param guess    生成的password字符数组
+     * @param index    当前递归方法调用所在字符数组的位置
+     * @param password 原始密码字符串（用来和生成的密码作比较）
+     */
+    private void generatePassword(char[] guess, int index, String password) {
+        for (short i = '0'; i <= 'z' && !found; i++) {
+            char c = (char) i;
+            if (!Character.isLetterOrDigit(c))
+                continue;
+            guess[index] = c;
+            if (index != guess.length - 1) {
+                generatePassword(guess, index + 1, password);
+            } else {
+                String guessPassword = new String(guess);
+                System.out.print(guessPassword + "\t");
+                if (guessPassword.equals(password)) { // 找到
+                    found = true;
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         StringOperator operator = new StringOperator();
         char[] randChars = new char[5];
@@ -73,6 +134,12 @@ public class StringOperator {
         System.out.println("Sort by StringOperator: ");
         operator.sort(tempRandStrings);
         System.out.println(Arrays.toString(tempRandStrings));
+
+        char[] passwordArray = new char[3];
+        operator.randomCharArray(passwordArray);
+        System.out.println("Password: " + new String(passwordArray));
+        System.out.println("Mapping Cyclically : " + operator.mappingPassword(new String(passwordArray)));
+        System.out.println("Mapping Recursively: " + operator.mappingPasswordRecursively(new String(passwordArray)));
     }
 
 }
