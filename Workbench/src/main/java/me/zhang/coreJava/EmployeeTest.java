5157c329c5df773f52b814e23966f25ba0f6d385
zhangxiangdong <zxdeinstein@163.com>
2017-10-06 16:42:56 +0800

    Break the encapsulation

--- Workbench/src/main/java/me/zhang/coreJava/EmployeeTest.java
+++ Workbench/src/main/java/me/zhang/coreJava/EmployeeTest.java
@@ -1,6 +1,7 @@
 package me.zhang.coreJava;
 
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by Zhang on 10/6/2017 3:41 PM.
@@ -19,6 +20,13 @@ public class EmployeeTest {
             employee.raiseSalary(5);
         }
         System.out.println(Arrays.deepToString(staff));
+
+        Employee carl = staff[0];
+        Date hireDay = carl.getHireDay();
+        double tenYearsInMilliSeconds = 10 * 365.25 * 24 * 60 * 60 * 1000;
+        hireDay.setTime((long) (hireDay.getTime() - tenYearsInMilliSeconds));
+        // give Carl ten years of added seniority
+        System.out.println(carl);
     }
 
 }
