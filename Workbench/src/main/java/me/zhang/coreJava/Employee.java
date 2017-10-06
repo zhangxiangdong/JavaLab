ed3d27096b5094d99c77855f0ca5f24d0c5fbd35
zhangxiangdong <zxdeinstein@163.com>
2017-10-06 16:02:01 +0800

    Separate classes

---
+++ Workbench/src/main/java/me/zhang/coreJava/Employee.java
@@ -0,0 +1,46 @@
+package me.zhang.coreJava;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Zhang on 10/6/2017 4:01 PM.
+ */
+public class Employee {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        this.hireDay = calendar.getTime();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Date getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(int byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", hireDay=" + hireDay +
+                '}';
+    }
+}
--- Workbench/src/main/java/me/zhang/coreJava/EmployeeTest.java
+++ Workbench/src/main/java/me/zhang/coreJava/EmployeeTest.java
@@ -1,8 +1,6 @@
 package me.zhang.coreJava;
 
 import java.util.Arrays;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 /**
  * Created by Zhang on 10/6/2017 3:41 PM.
@@ -23,43 +21,4 @@ public class EmployeeTest {
         System.out.println(Arrays.deepToString(staff));
     }
 
-}
-
-class Employee {
-    private String name;
-    private double salary;
-    private Date hireDay;
-
-    public Employee(String name, double salary, int year, int month, int day) {
-        this.name = name;
-        this.salary = salary;
-        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
-        this.hireDay = calendar.getTime();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public Date getHireDay() {
-        return hireDay;
-    }
-
-    public void raiseSalary(int byPercent) {
-        double raise = salary * byPercent / 100;
-        salary += raise;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                ", hireDay=" + hireDay +
-                '}';
-    }
 }
