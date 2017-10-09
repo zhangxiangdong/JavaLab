7a57da9e8e95a60d6fc498d82fbeafe3a7ff0592
zhangxiangdong <zxdeinstein@163.com>
2017-10-09 17:34:52 +0800

    Final

---
+++ Workbench/src/main/java/me/zhang/coreJava/Executive.java
@@ -0,0 +1,14 @@
+package me.zhang.coreJava;
+
+/**
+ * Created by zhangxiangdong on 2017/10/9.
+ * <p>
+ * Executive can't be extended.
+ */
+public final class Executive extends Manager {
+
+    public Executive(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+    }
+
+}
