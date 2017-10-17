1b56af0bad74c8479e4d283defda0063a73a74a4
zhangxiangdong <zxdeinstein@163.com>
2017-10-17 12:02:43 +0800

    Access outer state from inner class

---
+++ Workbench/src/main/java/me/zhang/coreJava/TalkingClock.java
@@ -0,0 +1,45 @@
+package me.zhang.coreJava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+/**
+ * Created by Zhang on 2017/10/17 11:28.
+ */
+public class TalkingClock {
+
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep) {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    public void start() {
+        // ActionListener listener = new TimePrinter(this); // parameter automatically added
+        ActionListener listener = new TimePrinter();
+        Timer timer = new Timer(interval, listener);
+        timer.start();
+    }
+
+    public class TimePrinter implements ActionListener {
+
+        // private TalkingClock outer;
+
+        // public TimePrinter(TalkingClock clock) { // automatically generated code
+        // outer = clock;
+        //}
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("At the tone, the time is " + new Date());
+            if (beep) { // outer.beep
+                Toolkit.getDefaultToolkit().beep();
+            }
+        }
+    }
+}
--- Workbench/src/main/java/me/zhang/coreJava/TimerTest.java
+++ Workbench/src/main/java/me/zhang/coreJava/TimerTest.java
@@ -1,10 +1,6 @@
 package me.zhang.coreJava;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Date;
 
 /**
  * Created by Zhang on 2017/10/17 10:57.
@@ -12,21 +8,11 @@ import java.util.Date;
 public class TimerTest {
 
     public static void main(String[] args) {
-        ActionListener listener = new TimePrinter();
-
-        Timer t = new Timer(10 * 1000, listener);
-        t.start();
+        TalkingClock clock = new TalkingClock(10 * 1000, false);
+        clock.start();
 
         JOptionPane.showMessageDialog(null, "Quit program?");
         System.exit(0);
     }
 
-    static class TimePrinter implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("At the tone, the time is " + new Date());
-            Toolkit.getDefaultToolkit().beep();
-        }
-    }
 }
