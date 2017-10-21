77762b1b703e462fcfd956e17cb8e33abcdc419a
zhangxiangdong <zxdeinstein@163.com>
2017-10-21 22:16:37 +0800

    Draw 2D graphs

--- Workbench/src/main/java/me/zhang/coreJava/gui/MyComponent.java
+++ Workbench/src/main/java/me/zhang/coreJava/gui/MyComponent.java
@@ -2,6 +2,9 @@ package me.zhang.coreJava.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 
 import static me.zhang.coreJava.gui.SimpleFrameTest.DEFAULT_HEIGHT;
 import static me.zhang.coreJava.gui.SimpleFrameTest.DEFAULT_WIDTH;
@@ -19,7 +22,30 @@ public class MyComponent extends JComponent {
         super.paintComponent(g);
         System.out.println("paintComponent");
 
-        g.drawString("Hello, World!", TEXT_X, TEXT_Y);
+        // g.drawString("Hello, World!", TEXT_X, TEXT_Y);
+        Graphics2D g2D = (Graphics2D) g;
+
+        double x = 100;
+        double y = 100;
+        double w = 200;
+        double h = 150;
+        Rectangle2D doubleRect = new Rectangle2D.Double(x, y, w, h);
+        Line2D doubleLine = new Line2D.Double(x, y, x + w, y + h);
+
+        Ellipse2D floatEllipse = new Ellipse2D.Float();
+        // floatEllipse.setFrameFromDiagonal(x, y, x + w, y + h);
+        floatEllipse.setFrame(doubleRect);
+
+        double centerX = doubleRect.getCenterX();
+        double centerY = doubleRect.getCenterY();
+        double radius = Math.sqrt(Math.pow(w, 2) + Math.pow(h, 2)) / 2;
+        Ellipse2D circle = new Ellipse2D.Double();
+        circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+
+        g2D.draw(doubleRect);
+        g2D.draw(doubleLine);
+        g2D.draw(floatEllipse);
+        g2D.draw(circle);
     }
 
     @Override
