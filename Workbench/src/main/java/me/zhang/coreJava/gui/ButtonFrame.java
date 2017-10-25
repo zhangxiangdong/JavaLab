280d88b5509556007999ee7d1b7989a5331af652
zhangxiangdong <zxdeinstein@163.com>
2017-10-25 16:35:37 +0800

    Customise actions

--- Workbench/src/main/java/me/zhang/coreJava/gui/ButtonFrame.java
+++ Workbench/src/main/java/me/zhang/coreJava/gui/ButtonFrame.java
@@ -2,6 +2,7 @@ package me.zhang.coreJava.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.EventHandler;
 
@@ -17,9 +18,15 @@ public class ButtonFrame extends JFrame {
 
         buttonPanel = new JPanel();
 
-        addButton("Red", Color.RED);
-        addButton("Green", Color.GREEN);
-        addButton("Blue", Color.BLUE);
+        // define actions
+        Action redAction = new ColorAction("Red", Color.RED);
+        Action greenAction = new ColorAction("Green", Color.GREEN);
+        Action blueAction = new ColorAction("Blue", Color.BLUE);
+
+        // add buttons for these actions
+        buttonPanel.add(new JButton(redAction));
+        buttonPanel.add(new JButton(greenAction));
+        buttonPanel.add(new JButton(blueAction));
 
         /* perform a method invocation action */
         JButton loadButton = new JButton("Load");
@@ -30,15 +37,44 @@ public class ButtonFrame extends JFrame {
         add(buttonPanel);
         pack();
 
+        // associate the R, G, and B keys with names
+        InputMap imap = buttonPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        imap.put(KeyStroke.getKeyStroke("ctrl R"), "panel.red");
+        imap.put(KeyStroke.getKeyStroke("ctrl G"), "panel.green");
+        imap.put(KeyStroke.getKeyStroke("ctrl B"), "panel.blue");
+
+        // associate the names with actions
+        ActionMap amap = buttonPanel.getActionMap();
+        amap.put("panel.red", redAction);
+        amap.put("panel.green", greenAction);
+        amap.put("panel.blue", blueAction);
+
         setLocationByPlatform(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    private void addButton(String label, Color color) {
-        JButton button = new JButton(label);
-        button.addActionListener((e) -> buttonPanel.setBackground(color));
-        buttonPanel.add(button);
+    public class ColorAction extends AbstractAction {
+
+        public static final String KEY_COLOR = "color";
+
+        /**
+         * Constructs a color action.
+         *
+         * @param name the name to show on the button
+         * @param c    the background color
+         */
+        public ColorAction(String name, Color c) {
+            putValue(Action.NAME, name);
+            putValue(Action.SHORT_DESCRIPTION, "Set panel color to " + name.toLowerCase());
+            putValue(KEY_COLOR, c);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            Color c = (Color) getValue(KEY_COLOR);
+            buttonPanel.setBackground(c);
+        }
     }
 
     /**
