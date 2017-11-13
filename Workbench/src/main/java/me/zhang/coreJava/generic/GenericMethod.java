9829bc32eb34257948308b240c877e45f44b21bf
zhangxiangdong <zxdeinstein@163.com>
2017-11-13 20:25:36 +0800

    Restrict type variable

--- Workbench/src/main/java/me/zhang/coreJava/generic/GenericMethod.java
+++ Workbench/src/main/java/me/zhang/coreJava/generic/GenericMethod.java
@@ -11,11 +11,29 @@ public class GenericMethod {
 
         // double middle = getMiddle(3.14, 2, 1000000000000L);
         System.out.println(getMiddle(3.14, 2, 1000000000000L));
+
+        System.out.println(min(2, 1, 4, 3, 7, 6, 5, 8, 9));
+        System.out.println(min(3.1, 2.0, 4.3));
     }
 
     @SafeVarargs
     private static <T> T getMiddle(T... a) {
         return a[a.length / 2];
+    }
+
+    @SafeVarargs
+    private static <T extends Number & Comparable> T min(T... a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+
+        T smallest = a[0];
+        for (T t : a) {
+            if (t.compareTo(smallest) < 0) {
+                smallest = t;
+            }
+        }
+        return smallest;
     }
 
 }
