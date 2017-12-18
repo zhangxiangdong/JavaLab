515b74e8aa79fe9f0fee670b1c5c5bb395841eff
zhangxiangdong <zxdeinstein@163.com>
2017-12-18 18:14:54 +0800

    Consider static factory methods instead of constructors

---
+++ Workbench/src/main/java/me/zhang/effective/CollectionsFactory.java
@@ -0,0 +1,26 @@
+package me.zhang.effective;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhangxiangdong on 2017/12/18.
+ */
+public class CollectionsFactory {
+
+    private CollectionsFactory() {
+    }
+
+    public static <E> List<E> newArrayList() {
+        return new ArrayList<>();
+    }
+
+    public static <K, V> Map<K, V> newHashMap() {
+        return new HashMap<>();
+    }
+
+    // ...
+
+}
---
+++ Workbench/src/test/java/me/zhang/effective/CollectionsFactoryTest.java
@@ -0,0 +1,27 @@
+package me.zhang.effective;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhangxiangdong on 2017/12/18.
+ */
+public class CollectionsFactoryTest {
+
+    @Test
+    public void newArrayList() {
+        List<String> newArrayList = CollectionsFactory.newArrayList();
+        Assert.assertTrue(newArrayList instanceof ArrayList);
+    }
+
+    @Test
+    public void newHashMap() {
+        Map<String, String> newHashMap = CollectionsFactory.newHashMap();
+        Assert.assertTrue(newHashMap instanceof HashMap);
+    }
+}
