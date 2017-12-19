7bf661f87f7258f05b388e037df04982fd30afaf
zhangxiangdong <zxdeinstein@163.com>
2017-12-19 17:48:56 +0800

    Enforce noninstantiability with a private constructor

---
+++ Workbench/src/main/java/me/zhang/effective/item4/Util.java
@@ -0,0 +1,13 @@
+package me.zhang.effective.item4;
+
+/**
+ * Created by zhangxiangdong on 2017/12/19.
+ */
+public class Util {
+
+    // Suppresses default constructor, ensuring non-instantiability.
+    private Util() {
+        throw new AssertionError();
+    }
+
+}
