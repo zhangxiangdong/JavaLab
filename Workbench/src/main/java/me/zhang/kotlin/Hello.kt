c9e6d81e8b534cfba4826d02c3584e8b28515470
zhangxiangdong <zxdeinstein@163.com>
2017-12-24 18:43:05 +0800

    Learn basic syntax of kotlin

--- Workbench/src/main/java/me/zhang/kotlin/Hello.kt
+++ Workbench/src/main/java/me/zhang/kotlin/Hello.kt
@@ -6,3 +6,119 @@ package me.zhang.kotlin
 fun main(args: Array<String>) {
     println("Hello World!")
 }
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun printSum(a: Int, b: Int) {
+    println("sum of $a and $b is ${a + b}")
+}
+
+fun minus(a: Int, b: Int) = a - b
+
+/* /* Unlike Java, block comments in Kotlin can be nested. */ */
+var x = 0
+
+fun incrementX() {
+    x += 1
+}
+
+fun stringTemplate() {
+    var a = 1
+    val s1 = "a is $a"
+    println(s1)
+
+    a = 2
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+    println(s2)
+}
+
+fun maxOf(a: Int, b: Int): Int {
+    return if (a > b) {
+        a
+    } else {
+        b
+    }
+}
+
+fun minOf(a: Int, b: Int) = if (a > b) b else a
+
+fun parseInt(n: String): Int? {
+    return n.toIntOrNull()
+}
+
+fun calcProduct(x: String, y: String): Int? {
+    val a = parseInt(x)
+    val b = parseInt(y)
+
+    return if (a != null && b != null) {
+        a * b
+    } else {
+        null
+    }
+}
+
+fun getStringLength(o: Any): Int? {
+    if (o is String) {
+        return o.length // 'o' is automatically cast to 'String'
+    }
+    // 'o' is not a 'String'
+    return null
+}
+
+fun loopAll(list: List<String>) {
+    for (item in list) {
+        println(item)
+    }
+    for (index in list.indices) {
+        println("(for)item at $index is ${list[index]}")
+    }
+    var index = 0
+    while (index < list.size) {
+        println("(while)item at $index is ${list[index]}")
+        index++
+    }
+}
+
+fun describe(o: Any): String = when (o) {
+    1 -> "One"
+    "Hello" -> "Greeting"
+    is Long -> "Long"
+    !is String -> "Not a string"
+    else -> "Unknown"
+}
+
+fun range() {
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+
+    val m = -1
+    if (m !in 0..3) {
+        println("$m not in 0, 1, 3")
+    }
+
+    for (x in 9 downTo 0 step 3) {
+        println(x)
+    }
+}
+
+fun lambda() {
+    val fruits = listOf("banana", "avocado", "apple", "kiwi")
+    fruits
+            .filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.substring(0, 1).toUpperCase() + it.substring(1) }
+            .forEach { println(it) }
+}
+
+fun obj() {
+    val list = ArrayList<String>() // no 'new' keyword required
+    list.add("M")
+    list.add("N")
+    list.add("O")
+    for (it in list) {
+        println(it)
+    }
+}
---
+++ Workbench/src/test/java/me/zhang/kotlin/HelloKtTest.kt
@@ -0,0 +1,111 @@
+package me.zhang.kotlin
+
+import org.junit.Test
+
+/**
+ * Created by Zhang on 12/24/2017 4:54 PM.
+ */
+class HelloKtTest {
+
+    @Test
+    fun sum() {
+        val a = 1
+        val b = 3
+        assert(me.zhang.kotlin.sum(a, b) == 4)
+    }
+
+    @Test
+    fun minus() {
+        val a = 3
+        val b = 2
+        assert(me.zhang.kotlin.minus(a, b) == 1)
+    }
+
+    @Test
+    fun printSum() {
+        me.zhang.kotlin.printSum(1, 2)
+    }
+
+    @Test
+    fun incrementX() {
+        me.zhang.kotlin.incrementX()
+        assert(me.zhang.kotlin.x == 1)
+    }
+
+    @Test
+    fun stringTemplate() {
+        me.zhang.kotlin.stringTemplate()
+    }
+
+    @Test
+    fun maxOf() {
+        val a = 2
+        val b = 4
+        assert(maxOf(a, b) == 4)
+    }
+
+    @Test
+    fun minOf() {
+        val a = 2
+        val b = 4
+        assert(minOf(a, b) == 2)
+    }
+
+    @Test
+    fun printProduct() {
+        var m = "3"
+        var n = "4"
+        assert(calcProduct(m, n) == 12)
+
+        m = "a"
+        assert(calcProduct(m, n) == null)
+
+        n = "b"
+        assert(calcProduct(m, n) == null)
+    }
+
+    @Test
+    fun getStringLength() {
+        val s = "A string"
+        assert(getStringLength(s) == 8)
+
+        val o = 1
+        assert(getStringLength(o) == null)
+    }
+
+    @Test
+    fun printAll() {
+        val list = listOf("A", "B", "C")
+        me.zhang.kotlin.loopAll(list)
+    }
+
+    @Test
+    fun describe() {
+        val one = 1
+        val greeting = "Hello"
+        val long = Long.MAX_VALUE
+        val notAString = 1.0f
+        val unknown = "unknown"
+        assert(me.zhang.kotlin.describe(one) == "One")
+        assert(me.zhang.kotlin.describe(greeting) == "Greeting")
+        assert(me.zhang.kotlin.describe(long) == "Long")
+        assert(me.zhang.kotlin.describe(notAString) == "Not a string")
+        assert(me.zhang.kotlin.describe(unknown) == "Unknown")
+    }
+
+    @Test
+    fun range() {
+        me.zhang.kotlin.range()
+    }
+
+    @Test
+    fun lambda() {
+        me.zhang.kotlin.lambda()
+    }
+
+    @Test
+    fun obj() {
+        me.zhang.kotlin.obj()
+    }
+
+}
