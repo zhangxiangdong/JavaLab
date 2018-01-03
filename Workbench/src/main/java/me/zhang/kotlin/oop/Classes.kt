326255edb31b6ab3de86a06d3a172ea4c78c9eb2
zhangxiangdong <zxdeinstein@163.com>
2018-01-03 14:24:22 +0800

    Interfaces and Visibility Modifiers

--- Workbench/src/main/java/me/zhang/kotlin/oop/Classes.kt
+++ Workbench/src/main/java/me/zhang/kotlin/oop/Classes.kt
@@ -42,6 +42,34 @@ private fun testConstructors() {
 
 // ************************* CLASSES ****************************
 
+open class Outer {
+    private val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4  // public by default
+
+    protected class Nested {
+        public val e: Int = 5
+    }
+}
+
+class Subclass : Outer() {
+    // a is not visible
+    // b, c and d are visible
+    // Nested and e are visible
+
+    override val b = 5   // 'b' is protected
+}
+
+class Unrelated(o: Outer) {
+    // o.a, o.b are not visible
+    // o.c and o.d are visible (same module)
+    // Outer.Nested is not visible, and Nested::e is not visible either
+    fun calc() {
+        // Outer.Nested()
+    }
+}
+
 const val SUBSYSTEM_DEPRECATED: String = "This subsystem is deprecated"
 
 object Const {
@@ -134,7 +162,17 @@ class C() : A(), B {
 
 interface Foo {
     val count: Int
+
+    val string: String
+        get() {
+            return "default string"
+        }
+
     fun f()
+
+    fun bar() {
+        println(string)
+    }
 }
 
 open class Bar1 : Foo {
