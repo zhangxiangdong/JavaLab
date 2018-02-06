b8dc703ec196bb8c82bd9e25d0540dd72b0b3c94
zhangxiangdong <zxdeinstein@163.com>
2018-02-06 16:12:27 +0800

    Extend AutoClosable

---
+++ Workbench/src/main/java/me/zhang/coreJava/utils/Utils.kt
@@ -0,0 +1,23 @@
+package me.zhang.coreJava.utils
+
+/**
+ * Created by zhangxiangdong on 2018/2/6.
+ */
+inline fun <T : AutoCloseable, R> T.use(block: (T) -> R): R {
+    var closed = false
+    try {
+        return block(this)
+    } catch (e: Exception) {
+        closed = true
+        try {
+            close()
+        } catch (closeException: Exception) {
+            e.addSuppressed(closeException)
+        }
+        throw e
+    } finally {
+        if (!closed) {
+            close()
+        }
+    }
+}
