aa3bbc25ea2be134a8b95aa0850bbc8ad119a437
zhangxiangdong <zxdeinstein@163.com>
2018-03-25 17:58:47 +0800

    Improve the app with the Application object

--- Ktor/src/main/kotlin/Main.kt
+++ Ktor/src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
-package main.kotlin
-
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
@@ -12,16 +14,20 @@ import io.ktor.server.netty.Netty
  * Created by Zhang on 3/25/2018 4:07 PM.
  */
 fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080, module = Application::module)
+    server.start(wait = true)
+}
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
     @Suppress("EXPERIMENTAL_FEATURE_WARNING")
-    val server = embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
-            }
-            get("/demo") {
-                call.respondText("HELLO WORLD!")
-            }
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+        get("/demo") {
+            call.respondText("HELLO WORLD!")
         }
     }
-    server.start(wait = true)
 }
