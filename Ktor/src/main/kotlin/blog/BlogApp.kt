dca7cf02b1719a0c40a6c9e362ebc8142e459714
zhangxiangdong <zxdeinstein@163.com>
2018-03-25 19:30:24 +0800

    Extract out configuration data

--- Ktor/src/main/kotlin/blog/BlogApp.kt
+++ Ktor/src/main/kotlin/blog/BlogApp.kt
@@ -1,3 +1,5 @@
+package blog
+
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -7,18 +9,12 @@ import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
 /**
  * Created by Zhang on 3/25/2018 4:07 PM.
  */
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080, module = Application::module)
-    server.start(wait = true)
-}
 
-fun Application.module() {
+fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
     @Suppress("EXPERIMENTAL_FEATURE_WARNING")
