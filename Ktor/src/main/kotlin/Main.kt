50a1bfeed9ffae17c59e177ec90c3140e43daaa7
zhangxiangdong <zxdeinstein@163.com>
2018-03-25 17:41:39 +0800

    Get started with Ktor

---
+++ Ktor/src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+package main.kotlin
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+/**
+ * Created by Zhang on 3/25/2018 4:07 PM.
+ */
+fun main(args: Array<String>) {
+    @Suppress("EXPERIMENTAL_FEATURE_WARNING")
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
