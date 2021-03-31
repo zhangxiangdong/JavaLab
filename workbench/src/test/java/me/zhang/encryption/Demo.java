ae08ac3606352d712ff76f4b5ac86f4c0bdb6677
zhangxiangdong <zxdeinstein@163.com>
2021-03-31 20:42:16 +0800

    Rsa 加密测试

--- workbench/src/test/java/me/zhang/encryption/Demo.java
+++ workbench/src/test/java/me/zhang/encryption/Demo.java
@@ -89,4 +89,85 @@ public class Demo {
         System.out.println("RSA私钥解密数据: " + new String(priDecryptBytes));
     }
 
+    @Test
+    public void testLargeContent() throws Exception {
+        StringBuilder value = new StringBuilder();
+        for (int i = 0; i <= 29; i++) {
+            value.append("18cm");
+        }
+        System.out.println("待加密内容长度: " + value.toString().length());
+
+        // 加密算法
+        String algorithm = "RSA";
+        // 转换模式
+        String transform = "RSA/ECB/PKCS1Padding";
+
+        // 实例化秘钥对生成器
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algorithm);
+        // 初始化，秘钥长度512~16384位，64倍数
+        keyPairGenerator.initialize(1024);
+        // 生成秘钥对
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        // 公钥
+        PublicKey publicKey = keyPair.getPublic();
+        System.out.println("RSA公钥: " + Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+
+        // 私钥
+        PrivateKey privateKey = keyPair.getPrivate();
+        System.out.println("RSA私钥: " + Base64.getEncoder().encodeToString(privateKey.getEncoded()));
+
+        // ------ 测试公钥加密，私钥解密 ------
+        Cipher cipher = Cipher.getInstance(transform);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] pubEncryptBytes = cipher.doFinal(value.toString().getBytes());
+        System.out.println("RSA公钥加密后数据: " + Base64.getEncoder().encodeToString(pubEncryptBytes));
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] priDecryptBytes = cipher.doFinal(pubEncryptBytes);
+        System.out.println("RSA私钥解密后数据: " + new String(priDecryptBytes));
+
+        // ------ 测试私钥加密，公钥解密 ------
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        byte[] priEncryptBytes = cipher.doFinal(value.toString().getBytes());
+        System.out.println("RSA私钥加密后数据: " + Base64.getEncoder().encodeToString(priEncryptBytes));
+
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+        byte[] pubDecryptBytes = cipher.doFinal(priEncryptBytes);
+        System.out.println("RSA公钥解密后数据: " + new String(pubDecryptBytes));
+    }
+
+    @Test
+    public void testLargeContent2() throws Exception {
+        StringBuilder value = new StringBuilder();
+        for (int i = 0; i <= 29; i++) {
+            value.append("18cm");
+        }
+        System.out.println("待加密内容长度: " + value.toString().length());
+
+        // 加密算法
+        String algorithm = "RSA";
+
+        // 实例化秘钥对生成器
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algorithm);
+        // 初始化，秘钥长度512~16384位，64倍数
+        keyPairGenerator.initialize(1024);
+        // 生成秘钥对
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        // 公钥
+        PublicKey publicKey = keyPair.getPublic();
+        System.out.println("RSA公钥: " + Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+        // 私钥
+        PrivateKey privateKey = keyPair.getPrivate();
+        System.out.println("RSA私钥: " + Base64.getEncoder().encodeToString(privateKey.getEncoded()));
+
+        // ------ 测试公钥加密，私钥解密 ------
+        String pubEncrypt= RsaUtil.encrypt(publicKey, value.toString());
+        System.out.println("RSA公钥加密后数据: " + pubEncrypt);
+
+        String priDecrypt = RsaUtil.decrypt(privateKey, pubEncrypt);
+        System.out.println("RSA私钥解密后数据: " + priDecrypt);
+
+    }
+
 }
